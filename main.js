9fedd5186b11a34d197dce02757b03f7a0b9a2bd
Numaira222 <77472414+Numaira222@users.noreply.github.com>
2022-04-17 14:52:18 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+
+mustacheX=0;
+mustacheY=0;
+
+
+
 function preload() {
 
 }
@@ -6,14 +12,40 @@ function setup() {
     canvas = createCanvas(350, 350);
     canvas.position(110.250);
     canvas.center();
+    video = createCapture(VIDEO);
+    video.hide();
+    tint_colour = "";
+    posenet=ml5.poseNet(video,modelLoaded);
+    posenet.on("pose",gotPoses);
     
+
+
 }
 
 function draw() { 
+    image(video, 0, 0, 400, 450);
+    tint(tint_colour);
 }
 
 
 function take_snapshot() {
 
 }
+
+function filter_tint() {
+    tint_colour = document.getElementById("colour_name").value;
+}
+function modelLoaded(){
+    console.log("Posenet has initialized");
+}
+
+function gotPoses(results){
+    if(results.length>0){
+        console.log(results);
+        mustacheX=results[0].pose.nose.x-10;
+        mustacheY=results[0].pose.nose.y-10;
+        console.log("mustache x="+mustacheX);
+        console.log("mustache y="+mustacheY);
+    }
+    }
 
