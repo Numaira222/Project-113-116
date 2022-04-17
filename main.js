53651cf78cb8977eb39b4d1e3a9de4fddd827ab3
Numaira222 <77472414+Numaira222@users.noreply.github.com>
2022-04-17 15:09:52 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ mustacheY=0;
 
 
 function preload() {
-
+    mustache=loadImage('https://i.postimg.cc/zXWyx0vV/moustache-PNG38.png');
 }
 
 function setup() {
@@ -25,11 +25,12 @@ function setup() {
 function draw() { 
     image(video, 0, 0, 400, 450);
     tint(tint_colour);
+    image(mustache,mustacheX,mustacheY,30,30);
 }
 
 
-function take_snapshot() {
-
+function takeSnapshot() {
+    save('myFilterImage.png');
 }
 
 function filter_tint() {
@@ -42,7 +43,7 @@ function modelLoaded(){
 function gotPoses(results){
     if(results.length>0){
         console.log(results);
-        mustacheX=results[0].pose.nose.x-10;
+        mustacheX=results[0].pose.nose.x-135;
         mustacheY=results[0].pose.nose.y-10;
         console.log("mustache x="+mustacheX);
         console.log("mustache y="+mustacheY);
